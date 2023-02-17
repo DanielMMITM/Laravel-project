1764b12ba2cc0a7e371e342ed33f50283c3b4fe1
edaniel <19121051@morelia.tecnm.mx>
2023-02-17 17:43:54 -0600

    Estilos, archivos blade, renderizar desde views

---
+++ blog/resources/views/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div class="container">
+
+        @foreach ($posts as $post)
+            <article>
+                <h1>{{ $post['title'] }}</h1>
+                <p>{{ $post['excerpt'] }}</p>
+            </article>
+        @endforeach
+
+        <article>
+            <h1>Curso de Laravel</h1>
+            <p>sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr</p>
+        </article>
+
+        <article>
+            <h1>Curso de Laravel</h1>
+            <p>sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr</p>
+        </article>
+
+        <article>
+            <h1>Curso de Laravel</h1>
+            <p>sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr gaegawega aefawef wg f ag e g awegawegaewgaweg sit comodo est nostrud fugiat amet labot afawegawaww4awkg rgawogaw faowefa awgagpaf woeagaowgawa gwgawega awgawegaw ogpqwkr</p>
+        </article>
+    </div>
+    
+</body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,17 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/index', function () {
+    $posts = [
+        [
+            'title' => 'Novedades de Laravel 9.',
+            'excerpt' => 'Cillum nulla magna ut laborist aeaeg agjwejga egjagjweg wegjawengg gjgja gjngjg ajgngj jap paoaf agumr jgjgit af fr g rgr gagrgagea',
+        ],
+        [
+            'title' => 'Curso de Laravel 9.',
+            'excerpt' => 'Cillum nulla magna ut laborist aeaeg agjwejga egjagjweg wegjawengg gjgja gjngjg ajgngj jap paoaf agumr jgjgit af fr g rgr gagrgagea',
+        ],
+        [
+            'title' => 'Manejo basico de Eloquent',
+            'excerpt' => 'Cillum nulla magna ut laborist aeaeg agjwejga egjagjweg wegjawengg gjgja gjngjg ajgngj jap paoaf agumr jgjgit af fr g rgr gagrgagea',
+        ],
+    ];
+
+    // return view('index', [
+    //     'posts' => $posts
+    // ]);
+
+    // return view('index')->with([
+    //     'posts' => $posts,
+    // ]);
+
+    //ESTO SE HACE PARA PASAR UNA SOLA VARIABLE, SE PUEDE ENCADENAR MAS METODOS WITH Y PASAR MAS VARIABLES
+    return view('index')->with('posts', $posts);
 });
